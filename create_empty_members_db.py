39461a25467eb314ad274519763c8062ab98cda6
Louis Dartez <louis.dartez@gmail.com>
2017-09-26 20:14:39 -0500

    fixed members table creation

---
+++ create_empty_members_db.py
@@ -0,0 +1,30 @@
+#! python
+
+if __name__ == "__main__":
+    import os
+    import sys
+    import sqlite3 as sql
+    import argparse
+    p = argparse.ArgumentParser()
+    p.add_argument('-f', '--force', action='store_true', dest='f',
+                   help='overwrite existing files')
+    args = p.parse_args()
+
+    dbname = 'members.db'
+
+    if os.path.exists(dbname) and not args.f:
+        print "database file already exists: {}".format(dbname)
+        sys.exit()
+    elif os.path.exists(dbname) and args.f:
+        os.remove(dbname)
+
+
+    conn = sql.connect(dbname)
+    c = conn.cursor()
+
+    #create table
+    query = '''CREATE TABLE members
+               (id INTEGER PRIMARY KEY, firstname text, lastname text, inst text, usrdb text)'''
+    c.execute(query)
+    conn.commit()
+    conn.close()
