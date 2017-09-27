6702c90eb7a8b04deb5dc35e0b9c116a50e58b1d
Louis Dartez <louis.dartez@gmail.com>
2017-09-26 22:17:20 -0500

    added create_empty_members_db.py and add_member.py db helper scripts

--- create_empty_members_db.py
+++ create_empty_members_db.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument('-f', '--force', action='store_true', dest='f',
                    help='overwrite existing files')
+    p.add_argument('--dbroot', type=str, default='db',
+                   help="db root path")
     args = p.parse_args()
 
-    dbname = 'members.db'
+    dbname = os.path.join(args.dbroot, 'members.db')
 
     if os.path.exists(dbname) and not args.f:
         print "database file already exists: {}".format(dbname)
@@ -24,7 +26,7 @@ if __name__ == "__main__":
 
     #create table
     query = '''CREATE TABLE members
-               (id INTEGER PRIMARY KEY, firstname text, lastname text, inst text, usrdb text)'''
+               (id INTEGER PRIMARY KEY, firstname text, lastname text, inst text, userdb text)'''
     c.execute(query)
     conn.commit()
     conn.close()
